ab29a351cb71cc60b3d341300903e8914d5dae25
abarzatamara <103967668+TamaraAbarza@users.noreply.github.com>
2022-06-20 20:47:38 -0300

    Add files via upload

--- veterinaria.sql
+++ veterinaria.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 03-06-2022 a las 06:43:22
+-- Tiempo de generación: 21-06-2022 a las 01:46:27
 -- Versión del servidor: 10.4.24-MariaDB
 -- Versión de PHP: 7.4.29
 
@@ -43,9 +43,11 @@ CREATE TABLE `cliente` (
 --
 
 INSERT INTO `cliente` (`idCliente`, `dni`, `nombre`, `apellido`, `telefono`, `direccion`, `contactoAlternativo`, `activo`) VALUES
-(5, 123, 'Tamara', 'Abarza', 2664340080, 'Quinto centenario', 2665456789, 1),
-(6, 41566432, 'Luciano', 'Cejas', 2664534322, 'falucho 543', 2665456780, 1),
-(7, 41321456, 'Tania', 'Aguero', 2664334455, 'Barrio atte1', 2664234567, 1);
+(5, 1234, 'Tamara', 'Abarza', 2664340080, 'Quinto centenario', 2665456766, 1),
+(7, 41321456, 'Tania', 'Aguero', 2664334455, 'Barrio atte1', 2664234567, 1),
+(8, 123456, 'Celeste', 'Vazquez', 2664352111, 'calle123', 1234, 1),
+(9, 39993664, 'luciano', 'cejas', 1234, 'calle123', 213123, 1),
+(13, 123, 'Lautaro', 'Lucero', 2663453212, 'calle 1235 ', 266453212, 1);
 
 -- --------------------------------------------------------
 
@@ -72,7 +74,10 @@ CREATE TABLE `mascota` (
 INSERT INTO `mascota` (`idMascota`, `alias`, `sexo`, `especie`, `raza`, `colorPelo`, `fechaNac`, `idCliente`, `activo`) VALUES
 (4, 'Chester', 'macho', 'perro', 'pointer inglés', 'chocolate con blanco', '2019-12-05', 5, 1),
 (5, 'Ambar', 'hembra', 'perro', 'mestiza', 'marron claro', '2020-06-12', 5, 1),
-(6, 'Luna', 'hembra', 'gato', 'siames', 'blanco', '2015-01-21', 7, 1);
+(6, 'Luna', 'hembra', 'gato', 'siames', 'blanco', '2014-01-21', 7, 1),
+(7, 'Negro', 'macho', 'Gato', 'Mestizo', 'Negro', '2014-06-14', 7, 1),
+(10, 'mishi', 'macho', 'gato', 'mestizo', 'naranja', '2022-06-02', 5, 1),
+(14, 'Akira', 'hembra', 'perro', 'caniche', 'blaco', '2018-06-15', 13, 1);
 
 -- --------------------------------------------------------
 
@@ -94,9 +99,10 @@ CREATE TABLE `tratamiento` (
 --
 
 INSERT INTO `tratamiento` (`idTratamiento`, `descripcion`, `medicamento`, `importe`, `tipoTratamiento`, `activo`) VALUES
-(3, 'se aplicó la vacuna parainfluenza canina', 'Nobivac', 1600, 'vacunación', 1),
+(3, 'se aplicó la vacuna parainfluenza canina', 'Nobivac', 1600, 'vacunación', 0),
 (4, 'se aplicó un desparasitante en gotas', 'Dermovital', 500, 'desparacitacion', 1),
-(5, 'se le realizó un baño, corte de pelo y un tratamiento para pulgas', 'Osspret', 1500, 'baño y corte de pelo', 1);
+(5, 'se le realizó un baño, corte de pelo y un tratamiento para pulgas', 'Osspret', 1500, 'baño y corte de pelo', 1),
+(6, 'aplicacion de vacuna antirrabica', 'Nobivac', 1230, 'vacunación', 1);
 
 -- --------------------------------------------------------
 
@@ -119,9 +125,9 @@ CREATE TABLE `visita` (
 --
 
 INSERT INTO `visita` (`idVisita`, `idMascota`, `fechaVisita`, `detalle`, `peso`, `idTratamiento`, `activo`) VALUES
-(10, 4, '2021-10-20', 'no presenta síntomas', 23, 3, 1),
 (11, 5, '2022-04-15', 'no presenta síntomas', 12, 3, 1),
-(14, 4, '2022-05-07', 'adelgazamiento', 20, 4, 0);
+(14, 4, '2022-05-07', 'adelgazamiento', 20, 4, 1),
+(18, 7, '2022-06-02', 'sin sintomas', 4, 6, 1);
 
 --
 -- Índices para tablas volcadas
@@ -164,25 +170,25 @@ ALTER TABLE `visita`
 -- AUTO_INCREMENT de la tabla `cliente`
 --
 ALTER TABLE `cliente`
-  MODIFY `idCliente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `idCliente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 
 --
 -- AUTO_INCREMENT de la tabla `mascota`
 --
 ALTER TABLE `mascota`
-  MODIFY `idMascota` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `idMascota` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT de la tabla `tratamiento`
 --
 ALTER TABLE `tratamiento`
-  MODIFY `idTratamiento` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `idTratamiento` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT de la tabla `visita`
 --
 ALTER TABLE `visita`
-  MODIFY `idVisita` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `idVisita` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
 
 --
 -- Restricciones para tablas volcadas
